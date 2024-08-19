da39d170ea4161600f85ffc451029b804e595ed3
inkfarer <inkfarer@inkfarer.com>
2024-08-19 14:43:12 +0300

    Add a window to the intermission graphic for a camera feed

--- src/graphics/pages/intermission/IntermissionGraphic.vue
+++ src/graphics/pages/intermission/IntermissionGraphic.vue
@@ -4,8 +4,9 @@
             COL 1
         </div>
         <large-separator direction="vertical" />
-        <div class="bg-panel">
+        <div class="bg-panel right-panel">
             <intermission-schedule />
+            <div class="bg-inset camera-border" />
         </div>
     </div>
 </template>
@@ -23,6 +24,20 @@ import IntermissionSchedule from './IntermissionSchedule.vue';
 
     > .bg-panel {
         padding: 40px 50px;
+        display: flex;
+        flex-direction: column;
+
+        &.right-panel {
+            $schedule-height: 720px;
+            // Will explode if the schedule's height changes, but I don't foresee that.
+            clip-path: polygon(0% 0%, 0% 100%, 53px 100%, 53px $schedule-height, calc(100% - 53px) $schedule-height, calc(100% - 53px) calc(100% - 53px), 50px calc(100% - 53px), 53px 100%, 100% 100%, 100% 0%);
+
+            > .camera-border {
+                margin-top: 40px;
+                margin-bottom: 10px;
+                height: 100%;
+            }
+        }
     }
 }
 </style>
