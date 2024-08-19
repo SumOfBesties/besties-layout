8f5b2538ba448d1f8f1d3cc2afa78237831fb28a
inkfarer <inkfarer@inkfarer.com>
2024-08-19 22:24:09 +0300

    Show the host on the intermission graphic

--- src/graphics/pages/intermission/IntermissionGraphic.vue
+++ src/graphics/pages/intermission/IntermissionGraphic.vue
@@ -24,13 +24,36 @@
                 />
             </div>
             <div class="bg-inset m-t-16 layout horizontal center-vertical">
-                <div class="host-name-display">
-                    <fitted-content
-                        style="width: 225px; margin-top: 12px"
-                        align="center"
-                    >
-                        hosty host
-                    </fitted-content>
+                <div class="host-name-display layout vertical center-vertical center-horizontal">
+                    <template v-if="currentHost == null">
+                        No Host!
+                    </template>
+                    <template v-else>
+                        <fitted-content
+                            class="host-name"
+                            align="center"
+                        >
+                            {{ currentHost.name }}
+                        </fitted-content>
+                        <div
+                            class="layout horizontal center-horizontal center-vertical m-t-2 max-width"
+                            style="padding: 0 4px;"
+                        >
+                            <country-flag
+                                v-if="currentHost.countryCode != null"
+                                :country-code="currentHost.countryCode"
+                                class="host-flag"
+                            />
+                            <fitted-content class="host-pronoun-wrapper">
+                                <badge
+                                    v-if="!isBlank(currentHost.pronouns)"
+                                    class="host-pronouns"
+                                >
+                                    {{ currentHost.pronouns }}
+                                </badge>
+                            </fitted-content>
+                        </div>
+                    </template>
                     <div class="host-name-label">H</div>
                 </div>
                 <div class="music-icon">♫</div>
@@ -65,16 +88,23 @@ import MediaBox from 'components/MediaBox.vue';
 import IntermissionPrizeDisplay from './IntermissionPrizeDisplay.vue';
 import DonationTotal from 'components/DonationTotal.vue';
 import OmnibarSlideRotation from 'components/omnibar/OmnibarSlideRotation.vue';
-import { provide } from 'vue';
+import { computed, provide } from 'vue';
 import {
     MaxOmnibarBidWarItemsInjectionKey,
     MaxOmnibarBidWarTitleWidthInjectionKey
 } from '../../../dashboard/helpers/Injections';
 import FittedContent from 'components/FittedContent.vue';
 import VfdPixelText from 'components/VfdPixelText.vue';
+import { useTalentStore } from 'client-shared/stores/TalentStore';
+import { isBlank } from 'client-shared/helpers/StringHelper';
+import Badge from 'components/Badge.vue';
+import CountryFlag from 'components/CountryFlag.vue';
 
 provide(MaxOmnibarBidWarItemsInjectionKey, 3);
 provide(MaxOmnibarBidWarTitleWidthInjectionKey, 200);
+
+const talentStore = useTalentStore();
+const currentHost = computed(() => talentStore.findTalentItemById(talentStore.currentHostId));
 </script>
 
 <style scoped lang="scss">
@@ -106,7 +136,7 @@ provide(MaxOmnibarBidWarTitleWidthInjectionKey, 200);
 
 .logos {
     justify-content: space-between;
-    margin: 40px 60px 0;
+    margin: 25px 60px 0;
 
     img {
         width: 250px;
@@ -143,7 +173,9 @@ provide(MaxOmnibarBidWarTitleWidthInjectionKey, 200);
     color: colors.$vfd-teal;
     font-weight: 700;
     position: relative;
-    height: 65px;
+    width: 250px;
+    height: 73px;
+    overflow: hidden;
 
     > .host-name-label {
         font-family: 'Roboto Condensed', sans-serif;
@@ -151,12 +183,33 @@ provide(MaxOmnibarBidWarTitleWidthInjectionKey, 200);
         background-color: colors.$vfd-teal;
         color: colors.$vfd-background;
         position: absolute;
-        bottom: -2px;
+        bottom: 0;
         left: -2px;
         padding: 0 5px;
         min-width: 18px;
         height: 20px;
-        line-height: 21px;
+    }
+
+    .host-name {
+        padding: 0 4px;
+        width: 100%;
+        font-weight: 700;
+    }
+
+    .host-flag {
+        height: 22px;
+    }
+
+    .host-flag + .host-pronoun-wrapper {
+        margin-left: 4px;
+    }
+
+    .host-pronoun-wrapper {
+        margin-top: -8px;
+    }
+
+    .host-pronouns {
+        font-size: 18px;
     }
 }
 
