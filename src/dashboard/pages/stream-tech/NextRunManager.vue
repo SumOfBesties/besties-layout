da73ef84959e3fb95cc529f26fb4c75fa3b2cd38
inkfarer <inkfarer@inkfarer.com>
2024-08-20 22:30:33 +0300

    Remind stream techs to complete interstitials and warn them if any stream titles are generated too long

--- src/dashboard/pages/stream-tech/NextRunManager.vue
+++ src/dashboard/pages/stream-tech/NextRunManager.vue
@@ -1,65 +1,63 @@
 <template>
-    <div>
-        <ipl-message
-            v-if="scheduleStore.nextSpeedrun == null"
-            type="info"
-        >
-            The next speedrun is unknown.
-        </ipl-message>
-        <ipl-space v-else>
-            <div class="m-t-4 m-b-2">
-                <div class="text-low-emphasis">Next run ({{ speedrunCount.current === -1 ? '?' : speedrunCount.current }}/{{ speedrunCount.total }})</div>
-                <div class="speedrun-name">{{ scheduleStore.nextSpeedrun.title }}</div>
-                <div class="m-b-8">
-                    <span>est. {{ formatDuration(scheduleStore.nextSpeedrun.estimate) }}</span>
-                    <template v-if="scheduleStore.nextSpeedrun.category != null">
-                        – {{ scheduleStore.nextSpeedrun.category }}
-                    </template>
-                </div>
-                <div>
-                    <font-awesome-icon icon="gamepad" size="sm" fixed-width />
-                    {{ talentStore.formatSpeedrunTeamList(scheduleStore.nextSpeedrun.teams) }}
-                </div>
-                <div v-if="scheduleStore.nextSpeedrun.commentatorIds.length > 0">
-                    <font-awesome-icon icon="headset" size="sm" fixed-width />
-                    {{ talentStore.formatTalentIdList(scheduleStore.nextSpeedrun.commentatorIds, 4) }}
-                </div>
+    <ipl-message
+        v-if="scheduleStore.nextSpeedrun == null"
+        type="info"
+    >
+        The next speedrun is unknown.
+    </ipl-message>
+    <ipl-space v-else>
+        <div class="m-t-4 m-b-2">
+            <div class="text-low-emphasis">Next run ({{ speedrunCount.current === -1 ? '?' : speedrunCount.current }}/{{ speedrunCount.total }})</div>
+            <div class="speedrun-name">{{ scheduleStore.nextSpeedrun.title }}</div>
+            <div class="m-b-8">
+                <span>est. {{ formatDuration(scheduleStore.nextSpeedrun.estimate) }}</span>
+                <template v-if="scheduleStore.nextSpeedrun.category != null">
+                    – {{ scheduleStore.nextSpeedrun.category }}
+                </template>
             </div>
-            <div class="speedrun-details m-b-8">
-                <ipl-data-row
-                    label="System"
-                    :value="`${scheduleStore.nextSpeedrun.system ?? 'Unknown'}${scheduleStore.nextSpeedrun.emulated ? ' (Emulated)' : ''}`"
-                />
-                <ipl-data-row
-                    label="Release year"
-                    :value="scheduleStore.nextSpeedrun.releaseYear"
-                />
-                <ipl-data-row
-                    label="Layout"
-                    :value="scheduleStore.nextSpeedrun.layout"
-                />
-                <ipl-data-row
-                    label="Relay?"
-                    :value="scheduleStore.nextSpeedrun.relay ? 'Yes' : 'No'"
-                />
-                <ipl-data-row
-                    label="Twitch category"
-                    :value="scheduleStore.activeSpeedrun?.twitchCategory?.name ?? 'N/A'"
-                    style="grid-column: span 2"
-                />
+            <div>
+                <font-awesome-icon icon="gamepad" size="sm" fixed-width />
+                {{ talentStore.formatSpeedrunTeamList(scheduleStore.nextSpeedrun.teams) }}
             </div>
-            <div class="text-center">
-                <ipl-button
-                    inline
-                    small
-                    @click="scheduleItemEditor?.openForNextSpeedrun"
-                >
-                    <font-awesome-icon icon="pen-to-square" />
-                    Edit next run
-                </ipl-button>
+            <div v-if="scheduleStore.nextSpeedrun.commentatorIds.length > 0">
+                <font-awesome-icon icon="headset" size="sm" fixed-width />
+                {{ talentStore.formatTalentIdList(scheduleStore.nextSpeedrun.commentatorIds, 4) }}
             </div>
-        </ipl-space>
-    </div>
+        </div>
+        <div class="speedrun-details m-b-8">
+            <ipl-data-row
+                label="System"
+                :value="`${scheduleStore.nextSpeedrun.system ?? 'Unknown'}${scheduleStore.nextSpeedrun.emulated ? ' (Emulated)' : ''}`"
+            />
+            <ipl-data-row
+                label="Release year"
+                :value="scheduleStore.nextSpeedrun.releaseYear"
+            />
+            <ipl-data-row
+                label="Layout"
+                :value="scheduleStore.nextSpeedrun.layout"
+            />
+            <ipl-data-row
+                label="Relay?"
+                :value="scheduleStore.nextSpeedrun.relay ? 'Yes' : 'No'"
+            />
+            <ipl-data-row
+                label="Twitch category"
+                :value="scheduleStore.activeSpeedrun?.twitchCategory?.name ?? 'N/A'"
+                style="grid-column: span 2"
+            />
+        </div>
+        <div class="text-center">
+            <ipl-button
+                inline
+                small
+                @click="scheduleItemEditor?.openForNextSpeedrun"
+            >
+                <font-awesome-icon icon="pen-to-square" />
+                Edit next run
+            </ipl-button>
+        </div>
+    </ipl-space>
 </template>
 
 <script setup lang="ts">
--- src/dashboard/pages/stream-tech/StreamTechPanel.vue
+++ src/dashboard/pages/stream-tech/StreamTechPanel.vue
@@ -2,7 +2,27 @@
     <div class="stream-tech-layout">
         <layout-manager />
         <active-run-manager />
-        <next-run-manager />
+        <div>
+            <next-run-manager />
+            <div class="alerts m-t-8">
+                <ipl-message
+                    v-if="showLongTwitchTitleWarning"
+                    type="warning"
+                    closeable
+                    class="m-t-8"
+                    @close="showLongTwitchTitleWarning = false"
+                >
+                    Generated a Twitch title that was too long! It should be manually edited to fit within the character limit.
+                </ipl-message>
+                <ipl-message
+                    v-if="showRemainingInterstitialsMessage"
+                    type="info"
+                    class="m-t-8"
+                >
+                    Remember to complete all interstitials before starting the next run.
+                </ipl-message>
+            </div>
+        </div>
         <rundown-display class="rundown-display" />
     </div>
     <schedule-item-editor ref="scheduleItemEditor" />
@@ -15,7 +35,7 @@
 import RundownDisplay from '../../components/RundownDisplay.vue';
 import ActiveRunManager from './ActiveRunManager.vue';
 import ScheduleItemEditor from '../../components/ScheduleItemEditor.vue';
-import { provide, ref } from 'vue';
+import { computed, provide, ref } from 'vue';
 import {
     CountrySelectDialogInjectionKey,
     ScheduleItemEditorInjectionKey,
@@ -26,6 +46,8 @@ import NextRunManager from './NextRunManager.vue';
 import LayoutManager from './LayoutManager.vue';
 import TalentItemEditDialog from '../../components/TalentItemEditDialog.vue';
 import TwitchCategorySearchDialog from '../../components/TwitchCategorySearchDialog.vue';
+import { IplMessage } from '@iplsplatoon/vue-components';
+import { useScheduleStore } from 'client-shared/stores/ScheduleStore';
 
 const scheduleItemEditor = ref<InstanceType<typeof ScheduleItemEditor>>();
 provide(ScheduleItemEditorInjectionKey, scheduleItemEditor);
@@ -35,6 +57,15 @@ const talentItemEditDialog = ref<InstanceType<typeof TalentItemEditDialog>>();
 provide(TalentItemEditDialogInjectionKey, talentItemEditDialog);
 const twitchCategorySearchDialog = ref<InstanceType<typeof TwitchCategorySearchDialog>>();
 provide(TwitchCategorySearchDialogInjectionKey, twitchCategorySearchDialog);
+
+const scheduleStore = useScheduleStore();
+
+const showLongTwitchTitleWarning = ref(false);
+nodecg.listenFor('twitch:generatedTitleTooLong', () => {
+    showLongTwitchTitleWarning.value = true;
+});
+const showRemainingInterstitialsMessage = computed(() =>
+    scheduleStore.interstitialsBeforeActiveRun.filter(interstitial => !interstitial.completed).length > 0);
 </script>
 
 <style lang="scss">
