efea5525afc3b9cfbe9f5cee2d809c328dac3c6c
inkfarer <inkfarer@inkfarer.com>
2024-08-20 13:03:09 +0300

    Rename the Settings panel to the OBS Settings panel

--- src/dashboard/obs-settings.ts
+++ src/dashboard/obs-settings.ts
@@ -5,10 +5,10 @@ import { createPinia } from 'pinia';
 import { setUpErrorHandler } from './helpers/ErrorHandlerStore';
 import { installCommonHelpers } from 'client-shared/helpers/InstallCommonHelpers';
 import { initObsStore } from 'client-shared/stores/ObsStore';
-import SettingsPanel from './pages/settings/SettingsPanel.vue';
+import ObsSettingsPanel from './pages/obs-settings/ObsSettingsPanel.vue';
 
 (async () => {
-    const app = createApp(SettingsPanel);
+    const app = createApp(ObsSettingsPanel);
     app.use(createPinia());
     setUpErrorHandler(app);
     installCommonHelpers(app, false);
